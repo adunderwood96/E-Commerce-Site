e779933c97ec4b6b6ee756267e9ea255fae52222
Ashley <adunderwood96@gmail.com>
2022-03-10 12:12:31 -0500

    fixed isssue with tags not seeding/being reg through mysql

--- models/Product.js
+++ models/Product.js
@@ -4,24 +4,23 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+// Initialize Product model (table) by extending off Sequelize Model class
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
+      allowNull: false,
       autoIncrement: true
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
-    price:{
+    price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
@@ -39,8 +38,8 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "category",
-        key: "id"
+        model: 'category',
+        key: 'id'
       }
     }
   },
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,33 +1,32 @@
+
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
+      allowNull: false,
       autoIncrement: true
     },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "product",
-        key: "id",
-      },
+        model: 'product',
+        key: 'id'
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "tag",
-        key: "id"
+        model: 'tag',
+        key: 'id'
       }
     }
-
   },
   {
     sequelize,
